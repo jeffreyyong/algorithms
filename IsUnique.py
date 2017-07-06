43a75fe99ebec5d05ae9fd5b06c663447b447308
Jeffrey Yong <jeffreyyong10@gmail.com>
2017-07-06 20:32:47 +0100

    Add IsUnique.py, haven't added the test yet

---
+++ IsUnique.py
@@ -0,0 +1,18 @@
+import unitest
+
+def unique(string):
+    if len(string) > 128:
+        return False
+
+    char_set = [False for _ in range(128)]
+
+    for char in string:
+        val = ord(char)
+        if char_set[val]:
+            return False
+        char_set[val] = True
+
+    return True
+
+class Test(unittest.TestCase):
+    dataT = [('abcd'), ('s4fad'), ('')]

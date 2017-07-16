e1228e06b2e8f41fd890ea61f3bf58c4a14e17c8
Jeffrey Yong <jeffreyyong10@gmail.com>
2017-07-16 16:23:50 +0100

    bottlecap3.py refactoring

--- BottleCaps/bottlecap3.py
+++ BottleCaps/bottlecap3.py
@@ -1,168 +1,147 @@
-capCount = 16
-capList = ["O", "X", "O", "O", "O", "O", "O", "X", "O", "O", "O", "X", "O", "O", "O", "O"]
+cap_count = 16
+cap_list = ["O", "X", "O", "O", "O", "O", "O", "X", "O", "O", "O", "X", "O", "O", "O", "O"]
 
+def output_cap_list(player_name):
+    if player_name == "player1":
+        player_name = str("\033[91m") + player_name + str("\033[0m")
+    elif player_name == "player2":
+        player_name = str("\033[0;32m") + player_name + str("\033[0m")
+    elif player_name == "beginning":
+        player_name = str("\033[90m") + player_name + str("\033[0m")
 
-def output_cap_list(playerName):
-    if playerName == "player1":
-        playerName = str("\033[91m") + playerName + str("\033[0m")
-    elif playerName == "player2":
-        playerName = str("\033[0;32m") + playerName + str("\033[0m")
-    elif playerName == "beginning":
-        playerName = str("\033[90m") + playerName + str("\033[0m")
-
-    capListStr = ""
-    for i in range(0, len(capList)):
-        cap = capList[i]
+    cap_list_str = ""
+    for i in range(0, len(cap_list)):
+        cap = cap_list[i]
         if cap == "P1":
-            capList[i] = "X"
-            capListStr += str("\033[91mX\033[0m")
+            cap_list[i] = "X"
+            cap_list_str += str("\033[91mX\033[0m")
         elif cap == "P2":
-            capList[i] = "X"
-            capListStr += str("\033[0;32mX\033[0m")
+            cap_list[i] = "X"
+            cap_list_str += str("\033[0;32mX\033[0m")
         elif cap == "X":
-            capListStr += "_"
+            cap_list_str += "_"
         else:
-            capListStr += cap
+            cap_list_str += cap
 
-    print(playerName + ": " + capListStr)
+    print(player_name + ": " + cap_list_str)
 
-    if capList.count(capList[0]) == len(capList):
-        print("*"*50 + "\n" + playerName + " WON!")
+    if cap_list.count(cap_list[0]) == len(cap_list):
+        print("*"*50 + "\n" + player_name + " WON!")
 
 
-# def validateForInput(selectedCaps):
-#     length = len(selectedCaps)
+# def validate_for_input(selected_caps):
+#     length = len(selected_caps)
 #     if length == 0 or length > 2:
 #         return False
 
-#     if int(selectedCaps[1]) - int(selectedCaps[0]) != 1:
+#     if int(selected_caps[1]) - int(selected_caps[0]) != 1:
 #         return False
 
-#     for index in selectedCaps:
+#     for index in selected_caps:
 #         try:
 #             i = int(index)
-#             if capList[i-1] != "0": return False
+#             if cap_list[i-1] != "0": return False
 #         except:
 #             return False
 
 #     return True
 
-def validateForInput(selectedCaps):
-    if len(selectedCaps) == 0 or len(selectedCaps) > 2: return False
+def validate_for_input(selected_caps):
+    if len(selected_caps) == 0 or len(selected_caps) > 2: return False
 
-    for one in selectedCaps:
+    for one in selected_caps:
         try:
             temp = int(one)
-            if capList[temp-1] != "O": return False
+            if cap_list[temp-1] != "O": return False
         except:
             return False
 
-    if len(selectedCaps) == 1: return True
-    if int(selectedCaps[1]) - int(selectedCaps[0]) != 1: return False
+    if len(selected_caps) == 1: return True
+    if int(selected_caps[1]) - int(selected_caps[0]) != 1: return False
 
     return True
 
 
-def removeSelectedCapsFromCapList(playerName, selectedCaps):
-    if playerName == "player1": markString = "P1"
-    if playerName == "player2": markString = "P2"
-    for position in selectedCaps:
-        capList[position-1] = markString
+def remove_select_caps(player_name, selected_caps):
+    if player_name == "player1": mark_string = "P1"
+    if player_name == "player2": mark_string = "P2"
+    for position in selected_caps:
+        cap_list[position-1] = mark_string
 
-    output_cap_list(playerName)
+    output_cap_list(player_name)
 
 
 
-def selectCapsForComputer(selectedCaps):
-    if selectedCaps[0] > 4 and selectedCaps[0] < 12:
-        for i in range(0, len(selectedCaps)):
-            if selectedCaps[i] > 7:
-                selectedCaps[i] = selectedCaps[i] - 4
+def select_caps_for_bot(selected_caps):
+    if selected_caps[0] > 4 and selected_caps[0] < 12:
+        for i in range(0, len(selected_caps)):
+            if selected_caps[i] > 7:
+                selected_caps[i] = selected_caps[i] - 4
             else:
-                selectedCaps[i] = selectedCaps[i] + 4
+                selected_caps[i] = selected_caps[i] + 4
 
-        return selectedCaps
+        return selected_caps
 
-    elif selectedCaps[0] == 1:
-        returnCaps = []
-        if capList[13] == "O" and capList[14] == "O":
-            returnCaps.extend([14,15])
+    elif selected_caps[0] == 1:
+        return_caps = []
+        if cap_list[13] == "O" and cap_list[14] == "O":
+            return_caps.extend([14,15])
         else:
             for i in range(12, 16):
-                if capList[i] == "O":
-                    returnCaps.append(i + 1)
-        return returnCaps
+                if cap_list[i] == "O":
+                    return_caps.append(i + 1)
+        return return_caps
     
     else:
-        returnCaps = []
-        if len(selectedCaps) == 1:
-            if capList[selectedCaps[0] - 2] == "O" and capList[selectedCaps[0] - 3] == "O":
-                returnCaps.extend([selectedCaps[0] - 2, selectedCaps[0] - 1])
-            elif selectedCaps[0] < 15 and capList[selectedCaps[0]] == "O" and capList[selectedCaps[0] + 1] == "O":
-                returnCaps.extend([selectedCaps[0] + 2, selectedCaps[0] + 1])
+        return_caps = []
+        if len(selected_caps) == 1:
+            if cap_list[selected_caps[0] - 2] == "O" and cap_list[selected_caps[0] - 3] == "O":
+                return_caps.extend([selected_caps[0] - 2, selected_caps[0] - 1])
+            elif selected_caps[0] < 15 and cap_list[selected_caps[0]] == "O" and cap_list[selected_caps[0] + 1] == "O":
+                return_caps.extend([selected_caps[0] + 2, selected_caps[0] + 1])
             else:
                 for i in range(12, 16):
-                    if capList[i] == "O" and i != selectedCaps[0] - 1:
-                        returnCaps.append(i + 1)
-                if len(returnCaps) == 0 and capList[0] == "O":
-                    returnCaps.append(1)
+                    if cap_list[i] == "O" and i != selected_caps[0] - 1:
+                        return_caps.append(i + 1)
+                if len(return_caps) == 0 and cap_list[0] == "O":
+                    return_caps.append(1)
         else:
-            if capList[selectedCaps[0] - 2] == "O":
-                returnCaps.append(selectedCaps[0] - 1)
-            elif capList[selectedCaps[0] + 1] == "O":
-                returnCaps.append(selectedCaps[0] + 2)
+            if cap_list[selected_caps[0] - 2] == "O":
+                return_caps.append(selected_caps[0] - 1)
+            elif cap_list[selected_caps[0] + 1] == "O":
+                return_caps.append(selected_caps[0] + 2)
             else:
-                if capList[0] == "O": returnCaps.append(1)
+                if cap_list[0] == "O": return_caps.append(1)
 
-        return returnCaps
+        return return_caps
 
 
-def endGame():
-    for cap in capList:
+def end_game():
+    for cap in cap_list:
         if cap != "X": return False
     return True
 
 
 # player the game
 
-output_cap_list("starting")
-capList[2] = "P1"
-capList[3] = "P2"
-output_cap_list("player1")
+def main():
+    output_cap_list("starting")
+    cap_list[2] = "P1"
+    cap_list[3] = "P2"
+    output_cap_list("player1")
 
-while(endGame() == False):
-    selectedCaps = []
-    while(validateForInput(selectedCaps) == False):
-        try:
-            selectedCaps = list(input("Please choose one or two caps(5, or 5,6): "))
-        except:
-            print("Try to choose again.")
-    removeSelectedCapsFromCapList("player2", selectedCaps)
+    while(end_game() == False):
+        selected_caps = []
+        while(validate_for_input(selected_caps) == False):
+            try:
+                selected_caps = list(input("Please choose one or two caps(5, or 5,6): "))
+            except:
+                print("Try to choose again.")
+        remove_select_caps("player2", selected_caps)
 
-    selectedCaps = selectCapsForComputer(selectedCaps)
+        selected_caps = select_caps_for_bot(selected_caps)
 
-    removeSelectedCapsFromCapList("player1", selectedCaps)
+        remove_select_caps("player1", selected_caps)
 
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-            
-            
-
-
-
-
-
-        
-
+if __name__ == '__main__':
+    main()

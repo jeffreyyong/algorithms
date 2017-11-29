655252931827ef09c2ea135894864cba7b1cb6c7
Jeffrey Yong <jeffreyyong10@gmail.com>
2017-11-29 22:41:32 +0000

    Finding the permutation palindrome

---
+++ leetcode/easy/palindrome_permutation.py
@@ -0,0 +1,27 @@
+'''
+Question:
+Given a string, determine if a permutation of the string could form a palindrome.
+
+For example,
+"code" -> False, "aab" -> True, "carerac" -> True.
+'''
+
+'''
+Answer:
+The idea is to iterate over string, adding current character to set if set doesn't cantain
+that character, or removing current character from set if set contains it.
+
+When the iteration is finished, just retrun set.size()==0 || set.size()==1
+
+set.size()==0 corresponds to the sutuation when there are even number of any character in the string
+set.size()==1 corresponds to the fact that there are even number of any character except one.
+
+'''
+
+import collections
+
+class Solution:
+
+    def can_permutate_palindrome(self, s):
+        return sum(v % 2 for v in collections.Counter(s).values()) < 2
+

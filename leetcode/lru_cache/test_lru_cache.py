5c9a6d95696f80ddf0d63f7268e56d98aa09eb72
Jeffrey Yong <jeffreyyong10@gmail.com>
2017-12-07 21:58:27 +0000

    Add test for lru cache

---
+++ leetcode/lru_cache/test_lru_cache.py
@@ -0,0 +1,28 @@
+import unittest
+
+from lru_cache import LRUCache
+
+class LRUCacheTest(unittest.TestCase):
+
+    def test_lru_cache(self):
+        cache = LRUCache(2)
+        
+        cache.put(1,1)
+        cache.put(2,2)
+        actual = cache.get(1)
+        self.assertEqual(1, actual)
+
+
+        cache.put(3,3)
+        actual = cache.get(2)
+        self.assertEqual(-1, actual)
+
+        cache.put(4,4)
+        actual = cache.get(1)
+        self.assertEqual(-1, actual)
+        actual = cache.get(3)
+        self.assertEqual(3, actual)
+        actual = cache.get(4)
+        self.assertEqual(4, actual)
+
+

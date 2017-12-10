9228e3f5744f8ab0a98ab35c3695a4441c8d8fa0
Jeffrey Yong <jeffreyyong10@gmail.com>
2017-12-11 07:56:42 +0800

    Add line item class

---
+++ leetcode/fp/06-dp-1class-func/classic_strategy.py
@@ -0,0 +1,18 @@
+# BEGIN CLASSIC_STRATEGT
+
+from abc import ABC, abstractmethod
+from collections import namedtuple
+
+Customer = namedtuple('Customer', 'name fidelity')
+
+class LineItem:
+
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+
+    def total(self):
+        return self.price * self.quantity
+
+

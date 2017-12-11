caa20b72337e1fff5a92a5963bf2b53b683d7f93
Jeffrey Yong <jeffreyyong10@gmail.com>
2017-12-11 08:02:31 +0800

    Add order class as the context that provides a service delegating computation to interchangeable components that implement alternative algorithms

--- leetcode/fp/06-dp-1class-func/classic_strategy.py
+++ leetcode/fp/06-dp-1class-func/classic_strategy.py
@@ -15,4 +15,29 @@ class LineItem:
     def total(self):
         return self.price * self.quantity
 
+class Order: # the Context
+
+    def __init__(self, customer, cart, promotion=None):
+        self.customer = customer
+        self.cart = list(cart)
+        self.promotion = promotion
+
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sum(item.total() for item in self.cart)
+        return self.__total
+
+    def due(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        fmt = '<Order total: {:.2f} due: {:.2f}>'
+        return fmt.format(self.total(), self.due())
+
+
+
 

e53b698ead4dcad8b6509607f9e99f29f6ab5366
Jeffrey Yong <jeffreyyong10@gmail.com>
2017-12-14 01:21:31 +0800

    First unique character in a string algorithm

---
+++ leetcode/easy/first_unique_character_in_a_string.py
@@ -0,0 +1,16 @@
+'''
+Given a string, find the first non-repeating character in it and return its index, if it
+doesn't exist, return -1
+
+Note: May assume the string contains only lowercase letters
+'''
+
+class Solution:
+
+    def first_uniq_char(self, s):
+        letters='abcdefghijklmnopqrstuvwxyz'
+        index = [s.index(l) for l in letters if s.count(l) == 1]
+        return min(index) if len(index) > 0 else -1
+        
+
+
---
+++ leetcode/easy/test_first_unique_character_in_a_string.py
@@ -0,0 +1,15 @@
+import unittest
+
+from first_unique_character_in_a_string import *
+
+class FirstUniqueCharTest(unittest.TestCase):
+
+    def test_first_unique_char(self):
+        solution = Solution()
+        actual = solution.first_uniq_char("leetcode")
+        self.assertEqual(0, actual)
+        actual = solution.first_uniq_char("loveleetcode")
+        self.assertEqual(2, actual)
+
+
+

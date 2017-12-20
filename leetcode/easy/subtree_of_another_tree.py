5be03071033985fc1028de636145a2ad3a16a670
Jeffrey Yong <jeffreyyong10@gmail.com>
2017-12-20 21:45:26 +0800

    Use advanced approach (merkle tree) to find the subtree in a tree

--- leetcode/easy/subtree_of_another_tree.py
+++ leetcode/easy/subtree_of_another_tree.py
@@ -67,6 +67,40 @@ class Solution:
         if not s: return False
         return self.is_subtree(s.left, t) or self.is_subtree(s.right, t)
 
+    '''
+    Solution2: Advanced approach, O(|s| + |t|) (Merkle hashing):
+    For each node in a tree, we can create node.merkle, a hash representing it's subtree.
+    This hash is formed by hashing the concatenation of the merkle of the left child,
+    the node's value, and the merkle of the right child.
+    Then, two trees are identical if and only if the merkle hash of thier roots are equal 
+    (except when there is a hash collision). From there, finding the answer is straightforward
+    simply check i fany node in s has node.merkle = t.merkle
+    '''
 
+    def is_subtree_advanced(self, s, t):
+        from hashlib import sha256
+        def hash_(x):
+            S = sha256()
+            S.update(x)
+            return S.hexdigest()
+
+        def merkle(node):
+            if not node:
+                return '#'
+            m_left = merkle(node.left)
+            m_right = merkle(node.right)
+            node.merkle = hash_(m_left = str(node.val) + m_right)
+            return node.merkle
+
+        merkle(s)
+        merkle(t)
+
+        def dfs(node):
+            if not node:
+                return False
+            return (node.merkle == t.merkle or
+                    dfs(node.left) or dfs(node.right))
+
+        return dfs(s)
 
 

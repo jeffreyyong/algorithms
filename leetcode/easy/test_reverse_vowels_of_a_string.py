b22517402fe1f5c55abc58587419a96befb194af
Jeffrey Yong <jeffreyyong10@gmail.com>
2018-01-11 21:25:17 +0000

    Test to reverse vowels of  a string

---
+++ leetcode/easy/test_reverse_vowels_of_a_string.py
@@ -0,0 +1,13 @@
+import unittest
+
+from reverse_vowels_of_a_string import *
+
+class ReverseVowelsTest(unittest.TestCase):
+
+    def test_reverse_vowels(self):
+
+        solution = Solution()
+        actual = solution.reverse_vowels("hello")
+        self.assertEqual("holle", actual)
+        actual = solution.reverse_vowels("leetcode")
+        self.assertEqual("leotcede", actual)

a3a1630e845ec0d8d7c51ca891f7f7123f47776a
Jeffrey Yong <jeffreyyong10@gmail.com>
2018-01-28 23:04:49 +0000

    first unique character using find and rfind

--- gs/first_non_repeating_character.py
+++ gs/first_non_repeating_character.py
@@ -1,45 +1,32 @@
 '''
-Given a string, find the first non-repeating character in it. For example, if the
-input string is "GeeksforGeeks", then output should be "f" and if input string is 
-"GeeksQuiz", then output should be 'G'
+Given a string, find the first non-repeating character in it and return its index, if it
+doesn't exist, return -1
+
+Note: May assume the string contains only lowercase letters
 '''
 
 '''
 Solution:
 
-We can use string characters as index and build a count array. Following is the algorithm
-1) Scan the string from left to right and construct the count array.
-2) Again, scan the string from left to right and check for count of each character, if
-    you find an element whose caount is 1, return it
-
 '''
 
-# Python program to print the first non-repeating character
-NO_OF_CHARS = 256
+import string
 
-# Returns the array of size 256 containing count of characters
-# in the passed char array
+class Solution:
 
-def get_char_count_array(string):
-    count = [0] * NO_OF_CHARS
-    for i in string:
-        count[ord(i)] += 1
-        return count
+    def first_unique_char(self, s):
 
+        min_index = len(s)
 
-# The function returns index of first non-repeating characters in a string.
-# If all characters are repeating then returns -1
+        for c in string.ascii_lowercase:
 
-def first_non_repeating(string):
+            curr_index = s.find(c)
 
-    count = get_char_count_array(string)
-    index = -1
-    k = 0
+            # if can find the character for the current index 
+            # and current index is the highest index for that particular character
+            # keep looking for the minimised index for all the characters
 
-    for i in string:
-        if count[ord(i)] == 1:
-            index = k
-            break
-        k += 1
+            if curr_index != -1 and curr_index == s.rfind(c):
+                min_index = min(min_index, curr_index)
 
-    return index
+        return min_index if min_index != len(s) else -1

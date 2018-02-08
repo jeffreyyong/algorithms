90ffba7f918c7d5cc5da681bd4c17b8eff3f133c
Jeffrey Yong <jeffreyyong10@gmail.com>
2018-02-08 22:15:37 +0000

    Majority element in array

---
+++ codility/majority_element_in_array.py
@@ -0,0 +1,67 @@
+'''
+
+
+A zero-indexed array A consisting of N integers is given. The dominator of array A is the value that occurs in more than half of the elements of A.
+
+For example, consider array A such that
+ A[0] = 3    A[1] = 4    A[2] =  3
+ A[3] = 2    A[4] = 3    A[5] = -1
+ A[6] = 3    A[7] = 3
+
+The dominator of A is 3 because it occurs in 5 out of 8 elements of A (namely in those with indices 0, 2, 4, 6 and 7) and 5 is more than a half of 8.
+
+Write a function
+
+    class Solution { public int solution(int[] A); }
+
+that, given a zero-indexed array A consisting of N integers, returns index of any element of array A in which the dominator of A occurs. The function should return −1 if array A does not have a dominator.
+
+Assume that:
+
+        N is an integer within the range [0..100,000];
+        each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+
+For example, given array A such that
+ A[0] = 3    A[1] = 4    A[2] =  3
+ A[3] = 2    A[4] = 3    A[5] = -1
+ A[6] = 3    A[7] = 3
+
+the function may return 0, 2, 4, 6 or 7, as explained above.
+
+Complexity:
+
+        expected worst-case time complexity is O(N);
+        expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+'''
+
+def solution(A):
+    candidate_ele = ''
+    candidate_cnt = 0
+
+    for value in A:
+        if candidate_ele == '':
+            candidate_ele = value
+            candidate_cnt = 1
+        else:
+            if value != candidate_ele:
+                candidate_cnt -= 1
+                if candidate_cnt == 0:
+                    candidate_ele = ''
+            else:
+                candidate_cnt += 1
+
+    if candidate_cnt == 0:
+        return -1
+
+    cnt = 0
+    last_idx = 0
+
+    for idx, value in enumerate(A):
+        if value == candidate_ele:
+            cnt += 1
+            last_idx = idx
+
+    if cnt > len(A)//2:
+        return last_idx
+
+    return -1

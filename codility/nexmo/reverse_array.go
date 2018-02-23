3df0f777fc547901d65babdd9142933f5d4f9d96
Jeffrey Yong <jeffreyyong10@gmail.com>
2018-02-23 21:52:24 +0000

    Find midpoint and reverse the array correspondingly

---
+++ codility/nexmo/reverse_array.go
@@ -0,0 +1,21 @@
+/*
+Problem: Given array A consisting of N intergers, return the reversed array.
+Solution: We can iterate over the first half of the array and exchange the elements with
+				  those in the second part of the array.
+*/
+
+package nexmo
+
+func Sum(x, y int) int {
+	return x + y
+}
+
+func reverse(A []int) []int {
+	N := len(A)
+
+	for i := 0; i < N/2; i++ {
+		k := N - i - 1
+		A[i], A[k] = A[k], A[i]
+	}
+	return A
+}

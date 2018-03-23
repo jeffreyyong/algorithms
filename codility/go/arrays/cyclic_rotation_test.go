2529df8626889c62ddac5ac34f0d12dc01d4ec9d
Jeffrey Yong <jeffreyyong10@gmail.com>
2018-03-23 23:45:49 +0000

    Cyclic rotation

---
+++ codility/go/arrays/cyclic_rotation_test.go
@@ -0,0 +1,13 @@
+package arrays
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCyclicRotation(t *testing.T) {
+	ans := cyclicRotation([]int{3, 8, 9, 7, 6}, 3)
+	if !reflect.DeepEqual(ans, []int{9, 7, 6, 3, 8}) {
+		t.Errorf("Error, got %d", ans)
+	}
+}

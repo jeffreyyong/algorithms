6d5f25b734d4b901bc0c1fdeb800a3aa48cae059
Jeffrey Yong <jeffreyyong10@gmail.com>
2018-04-12 22:10:41 +0100

    Odd occurrences in array

---
+++ codility/go/arrays/odd_occurrences_in_array_test.go
@@ -0,0 +1,12 @@
+package arrays
+
+import (
+	"testing"
+)
+
+func TestOddOccurrence(t *testing.T) {
+	ans := findOdd([]int{9, 3, 9, 3, 9, 7, 9})
+	if ans != 7 {
+		t.Errorf("Error, got %d", ans)
+	}
+}

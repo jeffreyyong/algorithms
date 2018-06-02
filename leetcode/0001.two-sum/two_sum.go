5dcd40a23d7ee80af284bf215062fb3c01c4c42e
Jeffrey Yong <jeffreyyong10@gmail.com>
2018-06-02 10:31:20 +0100

    Starting Leetcode journey

---
+++ leetcode/0001.two-sum/two_sum.go
@@ -0,0 +1,14 @@
+package problem0001
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int, len(nums))
+
+	for i, b := range nums {
+		if j, ok := m[target-b]; ok {
+			return []int{j, i}
+		}
+		m[b] = i
+	}
+	return []int{}
+
+}
